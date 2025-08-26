f82d0dd114f153e6b1c64295b1d9770a65e97de4
Koe10121 <6631501139@lamduan.mfu.ac.th>
2025-08-26 13:32:51 +0700

    initial commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+final baseUrl = 'http://localhost:3000';
+void main() async {
+  print('==== Login ====');
+  stdout.write('Username: ');
+  String? username = stdin.readLineSync()?.trim();
+  stdout.write('Password: ');
+  String? password = stdin.readLineSync()?.trim();
+  if (username == null || password == null) {
+    print('Username and password cannot be empty.');
+    return;
+  }
+
+  final url = Uri.parse("$baseUrl/login");
+  final response = await http.post(
+    url,
+    body: {'username': username, 'password': password},
+  );
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    var userId = data["userId"];
+    var username_loged = data["userName"];
+
+    while (true) {
+      print("========= Expense Tracking App =========");
+      print("Welcome $username_loged");
+      print("1. All expenses");
+      print("2. Today's expense");
+      print("3. Search expense");
+      print("4. Add new expense");
+      print("5. Delete an expense");
+      print("6. Exit");
+      stdout.write("Choose: ");
+      String? choice = stdin.readLineSync()?.trim();
+      if (choice == '6') {
+        print("----- Bye -----");
+        break;
+      } else if (choice == '1') {
+        await all_expenses(userId);
+      } else if (choice == '2') {
+        await today_expenses(userId);
+      } else if (choice == '3') {
+        await search_expense(userId);
+      } else if (choice == '4') {
+        await add_expense(userId);
+      } else if (choice == '5') {
+        await delete_expense(userId);
+      } else {
+        print("Invalid choice. Please try again.");
+      }
+    }
+  } else if (response.statusCode == 401 || response.statusCode == 500) {
+    final result = response.body;
+    print(result);
+  } else {
+    print("Unknown error");
+  }
+}
