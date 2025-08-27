4829fb29d88f54aee585d321b9e17d52d90191c1
Koe10121 <6631501139@lamduan.mfu.ac.th>
2025-08-27 11:09:54 +0700

    post/login

--- server/app.js
+++ server/app.js
@@ -5,3 +5,26 @@ const con = require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const sql = 'SELECT * FROM users WHERE username = ?';
+  con.query(sql, [username], (err, result) => {
+    if (err) {
+      return res.status(500).send('Server error');
+    }
+    if (result.length !== 1) { 
+      return res.status(401).send("Wrong username");
+    }
+    const user = result[0];
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) {
+        return res.status(500).send('Server error');
+      }
+      if (!isMatch) {
+        return res.status(401).send("Wrong password");
+      }
+      return res.json({ userId: user.id , userName: user.username});; 
+    });
+  });
+});
